6ba10f96d5e91e11dab4f8a9f4a6c4afed02a983
Federico Carrone <pyotrgalois@Federicos-MacBook-Pro.local>
2014-02-08 13:41:54 -0300

    initial commit

---
+++ init.el
@@ -0,0 +1,85 @@
+;; Prevent the cursor from blinking
+(blink-cursor-mode 0)
+;; Don't use messages that you don't read
+(setq initial-scratch-message "")
+(setq inhibit-startup-message t)
+;; Don't let Emacs hurt your ears
+
+;; You need to set `inhibit-startup-echo-area-message' from the
+;; customization interface:
+;; M-x customize-variable RET inhibit-startup-echo-area-message RET
+;; then enter your username
+(setq inhibit-startup-echo-area-message "guerry")
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+
+(load-theme 'misterioso)
+
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(require 'cl)
+;; Guarantee all packages are installed on start
+(defvar packages-list
+  '(rainbow-mode
+    rainbow-delimiters
+    multiple-cursors
+    whitespace-cleanup-mode
+    restclient
+    httprepl
+    smartparens
+    flycheck
+    projectile
+    project-explorer
+    yasnippet
+    jedi
+    ido-vertical-mode
+    ace-jump-mode
+    highlight-indentation
+    highlight-symbol
+    smex
+    undo-tree
+    flx-ido
+    rvm)
+  "List of packages needs to be installed at launch")
+
+(defun has-package-not-installed ()
+  (loop for p in packages-list
+        when (not (package-installed-p p)) do (return t)
+        finally (return nil)))
+(when (has-package-not-installed)
+  ;; Check for new packages (package versions)
+  (message "%s" "Get latest versions of all packages...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; Install the missing packages
+  (dolist (p packages-list)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(add-to-list 'load-path user-emacs-directory)
+(require 'sane-defaults)
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(require 'ido)
+(ido-mode 1)
+(ido-everywhere 1)
+
+(require 'flx-ido)
+(flx-ido-mode 1)
+(setq ido-use-faces nil)
+
+(projectile-global-mode)
+(ido-vertical-mode 1)
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)

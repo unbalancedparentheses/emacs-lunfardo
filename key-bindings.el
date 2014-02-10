a3dffc289355e7ce82752c8adfb8814a1371755f
Federico Carrone <federico.carrone@gmail.com>
2014-02-10 16:30:55 -0300

    ace word mode instead of per character

--- key-bindings.el
+++ key-bindings.el
@@ -24,5 +24,5 @@
 (global-set-key (kbd "C-c c") 'comment-or-uncomment-region)
 (global-set-key (kbd "C-c u") 'uncomment-region)
 
-(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+(global-set-key (kbd "C-c SPC") 'ace-jump-word-mode)
 (provide 'key-bindings)

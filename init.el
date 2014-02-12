132566bb4b06958fcb2341f99dc67c5507716033
Federico Carrone <pyotrgalois@Federicos-MacBook-Pro.local>
2014-02-12 11:47:52 -0300

    deleted theme security

--- init.el
+++ init.el
@@ -36,16 +36,3 @@
 (global-whitespace-cleanup-mode 1)
 
 (require 'erlang-brew)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

db064bd8c02f40b2f9c20c1ccdb483225e1a81de
pyotrgalois <federico.carrone@gmail.com>
2014-07-04 18:23:46 -0300

    moved core files to core dir

--- init.el
+++ init.el
@@ -6,30 +6,12 @@
 
 (defvar lunfardo-dir (file-name-directory load-file-name))
 (defvar lunfardo-core-dir (expand-file-name "core" lunfardo-dir))
-
-(add-to-list 'load-path user-emacs-directory)
 (add-to-list 'load-path lunfardo-core-dir)
 
 (require 'lunfardo-packages)
+(require 'lunfardo-ui)
+(require 'lunfardo-keychords)
+(require 'lunfardo-ido)
 
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
-
-(require 'sane-defaults)
-(require 'lunfardo-ui)
-
-(require 'lunfardo-ido)
-(require 'multiple-cursors)
-(require 'expand-region)
-(require 'key-bindings)
-(require 'lunfardo-keychords)
-
-(require 'erlang-brew)
-
-(smartparens-global-mode 1)
-(projectile-global-mode)
-(global-auto-complete-mode 1)
-(global-whitespace-cleanup-mode 1)
-(helm-mode 1)
-(global-hl-line-mode 1)
-(global-visual-line-mode 1)

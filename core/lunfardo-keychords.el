3ea1146a416317dbbf37e25912ed5c93980421f1
Federico Carrone <federico.carrone@gmail.com>
2014-08-25 11:24:13 -0300

    find file keychord

--- core/lunfardo-keychords.el
+++ core/lunfardo-keychords.el
@@ -1,5 +1,6 @@
 (require 'key-chord)
 
+(key-chord-define-global "FF" 'find-file)
 (key-chord-define-global "jj" 'ace-jump-word-mode)
 (key-chord-define-global "jl" 'ace-jump-line-mode)
 (key-chord-define-global "jk" 'ace-jump-char-mode)

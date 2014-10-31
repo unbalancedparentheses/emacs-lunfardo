81813ee6274eede98bf73b4c81eb36f7d5a01bc1
unbalancedparentheses <federico.carrone@gmail.com>
2014-10-31 17:50:43 -0300

    erlang indent level set to 2

--- modules/lunfardo-erlang.el
+++ modules/lunfardo-erlang.el
@@ -5,6 +5,8 @@
 (add-to-list 'auto-mode-alist '("\\.erl\\'" . erlang-mode))
 (add-to-list 'auto-mode-alist '("\\.app.src\\'" . erlang-mode))
 
+(setq erlang-indent-level 2)
+
 (add-hook 'erlang-mode-hook #'aggressive-indent-mode)
 
 (provide 'lunfardo-erlang)

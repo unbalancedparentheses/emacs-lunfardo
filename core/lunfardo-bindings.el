a9b575608ff7198c92ec5abbe5502a09c588c325
unbalancedparentheses <federico.carrone@gmail.com>
2014-11-24 02:59:45 -0300

    updating bindings to match most of sublime bindings

--- core/lunfardo-bindings.el
+++ core/lunfardo-bindings.el
@@ -4,6 +4,10 @@
 (global-set-key (kbd "C-<tab>") 'switch-to-previous-buffer)
 
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "s-P") 'helm-M-x)
+
+(global-set-key (kbd "s-p") 'helm-projectile)
+(global-set-key (kbd "C-M-p") 'helm-projectile)
 
 ;; Font size
 (global-set-key (kbd "C-+") 'text-scale-increase)
@@ -42,26 +46,38 @@
 (global-set-key (kbd "s-G") 'helm-google-suggest)
 (global-set-key (kbd "s-H") 'helm-descbinds)
 (global-set-key (kbd "s-I") 'find-user-init-file)
-(global-set-key (kbd "s-P") 'projectile-switch-project)
-(global-set-key (kbd "s-T") 'git-timemachine)
 (global-set-key (kbd "s-Y") 'gist-region-private)
 (global-set-key (kbd "s-d") 'mc/mark-next-like-this)
+
+(global-set-key (kbd "s-S-SPC") 'er/expand-region)
 (global-set-key (kbd "s-e") 'er/expand-region)
+
 (global-set-key (kbd "s-f") 'helm-swoop)
 (global-set-key (kbd "s-h") 'anzu-query-replace)
 (global-set-key (kbd "s-k") 'lunfardo-kill-whole-line)
 (global-set-key (kbd "s-l") 'goto-line)
 (global-set-key (kbd "s-m") 'magit-status)
 (global-set-key (kbd "s-o") 'helm-find-files)
-(global-set-key (kbd "s-p") 'helm-projectile)
-(global-set-key (kbd "s-r") 'undo-tree-redo)
-(global-set-key (kbd "s-t") 'helm-semantic-or-imenu)
+(global-set-key (kbd "s-r") 'helm-semantic-or-imenu)
 (global-set-key (kbd "s-u") 'undo-tree-visualize)
 (global-set-key (kbd "s-w") 'kill-this-buffer)
 (global-set-key (kbd "s-x") 'exchange-point-and-mark)
 (global-set-key (kbd "s-y") 'helm-show-kill-ring)
+(global-set-key (kbd "s-n") 'generate-buffer)
+
 (global-set-key (kbd "s-z") 'undo-tree-undo)
+(global-set-key (kbd "s-Z") 'undo-tree-redo)
+
+(global-set-key (kbd "s-t") 'helm-buffers-list)
+
+(global-set-key (kbd "s-T") 'reopen-killed-file)
+
+(global-set-key (kbd "C-q") 'kmacro-start-macro)
+(global-set-key (kbd "C-S-q") 'kmacro-end-or-call-macro)
 
 (global-set-key [tab] 'indent-or-complete)
+(global-set-key [escape] 'keyboard-escape-quit)
+
+(global-set-key (kbd "s-<mouse-1>") 'mc/add-cursor-on-click)
 
 (provide 'lunfardo-bindings)

7ca40b613a1dea9fff69804393a1bc39e2078d3c
unbalancedparentheses <federico.carrone@gmail.com>
2014-12-16 00:36:00 -0300

    bigger company idle time

--- core/lunfardo-company.el
+++ core/lunfardo-company.el
@@ -1,6 +1,6 @@
 (require 'company)
 
-(setq company-idle-delay 0.05)
+(setq company-idle-delay 0.10)
 (setq company-minimum-prefix-length 1)
 (setq company-show-numbers t)
 (setq company-tooltip-limit 20)

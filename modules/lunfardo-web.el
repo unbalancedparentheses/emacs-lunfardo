6e62b45e5a14111bf97e01aebd257ced5c2db8b6
Federico Carrone <federico.carrone@gmail.com>
2015-02-19 15:01:32 +0000

    associated web mode with html files

--- modules/lunfardo-web.el
+++ modules/lunfardo-web.el
@@ -1,3 +1,9 @@
 (lunfardo-require-packages '(web-mode))
 
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
 (provide 'lunfardo-web)

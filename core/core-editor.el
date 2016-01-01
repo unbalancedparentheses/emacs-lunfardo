cd17d2989fb64fd363979b4bd29e2763aadcaba1
unbalancedparentheses <federico.carrone@gmail.com>
2016-01-01 12:50:49 -0300

    Move prettify symbols unprettify at point before mode

--- core/core-editor.el
+++ core/core-editor.el
@@ -1,5 +1,5 @@
-(global-prettify-symbols-mode 1)
-(setq prettify-symbols-unprettify-at-point 'right-edge)
+(setq prettify-symbols-unprettify-at-point t)
+(global-prettify-symbols-mode 1)q
 
 ;; cleanup whitespace on save
 (use-package whitespace-cleanup-mode

6cf9db4c7ed2dd04c71a096f4dbe1aa2096a87e6
unbalancedparentheses <federico.carrone@gmail.com>
2016-02-04 21:31:41 -0300

    Move helm code to core-helm

--- core/core-editor.el
+++ core/core-editor.el
@@ -86,21 +86,6 @@
   (define-key ivy-minibuffer-map (kbd "<C-tab>") 'ivy-next-line)
   (define-key ivy-minibuffer-map (kbd "<C-S-tab>") 'ivy-previous-line))
 
-(use-package helm
-  :init
-  (setq helm-M-x-fuzzy-match t
-	helm-buffers-fuzzy-matching t
-	helm-recentf-fuzzy-match t)
-  :bind
-  (("M-y" . helm-show-kill-ring)
-   )
-  :ensure t)
-
-(use-package helm-descbinds
-  :bind
-  (("C-h b" . helm-descbinds))
-  :ensure t)
-
 (use-package ag
   :ensure t)
 

696f42126d9b4c1e0f270499dd6262a3b45de95e
unbalancedparentheses <federico.carrone@gmail.com>
2016-02-04 21:30:55 -0300

    Add helm ag

--- core/core-helm.el
+++ core/core-helm.el
@@ -1,1 +1,19 @@
+(use-package helm
+  :init
+  (setq helm-M-x-fuzzy-match t
+	helm-buffers-fuzzy-matching t
+	helm-recentf-fuzzy-match t)
+  :bind
+  (("M-y" . helm-show-kill-ring)
+   )
+  :ensure t)
+
+(use-package helm-descbinds
+  :bind
+  (("C-h b" . helm-descbinds))
+  :ensure t)
+
+(use-package helm-ag
+  :ensure t)
+
 (provide 'core-helm)

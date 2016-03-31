539a0aa17492e3711ed15b7b9257aa39aa89aebc
unbalancedparentheses <federico.carrone@gmail.com>
2016-03-31 13:00:22 -0300

    Fix type for elixir mode

--- layers/+langs/elixir/config.el
+++ layers/+langs/elixir/config.el
@@ -1,5 +1,8 @@
-(use-package elixir
+(use-package elixir-mode
   :ensure t)
 
 (use-package alchemist
+  :config
+  (setq alchemist-hooks-test-on-save t)
+  (setq alchemist-hooks-compile-on-save t)
   :ensure t)

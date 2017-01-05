6f558eed88e013605bc097e4d3578847f5ff9bba
unbalancedparentheses <federico.carrone@gmail.com>
2017-01-05 18:15:26 -0300

    Fix order of functions in erlang layer

--- layers/+langs/erlang/config.el
+++ layers/+langs/erlang/config.el
@@ -1,45 +1,3 @@
-(require 'ivy-erlang-complete)
-
-(defun fix-erlang-project-includes (project-root)
-  "Find erlang include paths for PROJECT-ROOT with project deps."
-  (setq-local flycheck-erlang-include-path
-              (append
-               (s-split
-                "\n"
-                (shell-command-to-string
-                 (concat "find "
-                         project-root
-                         "/*"
-                         " -type d -name include"))
-                t)
-               (list project-root
-                     (concat project-root "/include")
-                     (concat project-root "/deps")
-                     default-directory
-                     (concat
-                      (locate-dominating-file
-                       default-directory
-                       "src") "include")
-                     (concat
-                      (locate-dominating-file
-                       default-directory
-                       "src") "deps")))))
-
-(defun fix-erlang-project-code-path (project-root)
-  "Find erlang include paths for PROJECT-ROOT with project deps."
-  (let ((code-path
-           (split-string (shell-command-to-string
-                        (concat "find " project-root " -type d -name ebin")))
-         ))
-    (setq-local flycheck-erlang-library-path code-path)))
-
-(defun my-erlang-hook ()
-  "Setup for erlang."
-  (let ((project-root (ivy-erlang-complete-autosetup-project-root)))
-      (fix-erlang-project-code-path project-root)
-      (fix-erlang-project-includes project-root))
-  (ivy-erlang-complete-init))
-
 (use-package company-erlang
   :ensure t)
 
@@ -54,13 +12,56 @@
             ("=>" . 8658) ; ⇒
             )))
   (setq flycheck-erlang-include-path '("../include" "../deps"))
+
+  (require 'ivy-erlang-complete)
+
+  (defun fix-erlang-project-includes (project-root)
+    "Find erlang include paths for PROJECT-ROOT with project deps."
+    (setq-local flycheck-erlang-include-path
+                (append
+                 (s-split
+                  "\n"
+                  (shell-command-to-string
+                   (concat "find "
+                           project-root
+                           "/*"
+                           " -type d -name include"))
+                  t)
+                 (list project-root
+                       (concat project-root "/include")
+                       (concat project-root "/deps")
+                       default-directory
+                       (concat
+                        (locate-dominating-file
+                         default-directory
+                         "src") "include")
+                       (concat
+                        (locate-dominating-file
+                         default-directory
+                         "src") "deps")))))
+
+  (defun fix-erlang-project-code-path (project-root)
+    "Find erlang include paths for PROJECT-ROOT with project deps."
+    (let ((code-path
+           (split-string (shell-command-to-string
+                          (concat "find " project-root " -type d -name ebin")))
+           ))
+      (setq-local flycheck-erlang-library-path code-path)))
+
+  (defun setup-ivy-erlang-complete ()
+  "Setup for erlang."
+  (let ((project-root (ivy-erlang-complete-autosetup-project-root)))
+      (fix-erlang-project-code-path project-root)
+      (fix-erlang-project-includes project-root))
+  (ivy-erlang-complete-init))
+
   (add-hook 'erlang-mode-hook 'add-pretty-lambda)
   (add-hook 'erlang-mode-hook (lambda () (run-hooks 'prog-mode-hook)))
   (add-hook 'erlang-mode-hook 'flycheck-mode)
+  (add-hook 'erlang-mode-hook #'setup-ivy-erlang-complete)
   (add-hook 'erlang-mode-hook #'company-erlang-init)
-  (add-hook 'erlang-mode-hook #'my-erlang-hook)
 
-   :mode
+  :mode
   ("\\.erl\\'" . erlang-mode)
   ("\\.hrl\\'" . erlang-mode)
   :ensure t)

30f2f790ebdc09665ca5afff99ddd027ce6d276d
Federico Carrone <unbalancedparen@hendrix.fibertel.com.ar>
2021-01-01 21:26:37 -0300

    Update init.el

--- init.el
+++ init.el
@@ -5,13 +5,6 @@
 ;;;
 ;;; code:
 
-
-;; path should start on the home directory
-;; expand region
-;; anzu
-;; evil mode
-
-;; multiple-cursors-mode
 ;; browse-kill-ring
 ;; show-paren-model
 ;; lsp-mode
@@ -21,11 +14,7 @@
 ;; always load newest byte code
 (setq load-prefer-newer t)
 
-;; welcome message
-(setq initial-scratch-message "")
-(setq inhibit-startup-message t)
-(setq inhibit-startup-echo-area-message 'current-user)
-
+;;;;;
 ;; package
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -49,14 +38,62 @@
 (use-package crux
   :ensure t)
 
-(use-package smartparens
-  :init (smartparens-global-mode)
+;;;;;
+;; ui
+
+;; start maximized
+(toggle-frame-maximized)
+
+;; theme
+(use-package dracula-theme
+  :init
+  (load-theme 'dracula t)
   :ensure t)
 
+;; no blinking cursor
+(blink-cursor-mode -1)
 
+;; removes nasty bell
+(setq ring-bell-function 'ignore)
 
-;;;
+;; show me empty lines after buffer end
+(set-default 'indicate-empty-lines t)
+(setq-default truncate-lines t)
+
+;; default font size
+(set-face-attribute 'default nil :height 180)
+
+(global-visual-line-mode 1)
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+
+(setq frame-title-format '("%b (%m)"))
+
+;; see matching pairs of parentheses and other characters
+(show-paren-mode t)
+
+(use-package rainbow-delimiters
+  :init
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+  :ensure t)
+
+(global-visual-line-mode 1)
+
+;;;;;
 ;; autocomplete
+
+(use-package ivy
+  :init
+  (ivy-mode 1)
+  :config
+  (setq ivy-use-virtual-buffers t)
+  :ensure t)
+
+(use-package counsel
+  :ensure t)
+
 (use-package company
   :init (global-company-mode)
 
@@ -82,16 +119,19 @@
   (company-statistics-mode)
   :ensure t)
 
-;;;
+;;;;;
+;; editor
 
+;; welcome message
+(setq initial-scratch-message "")
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message 'current-user)
 
-;; start maximized
-(toggle-frame-maximized)
-
-;; Revert (update) buffers automatically when underlying files are changed externally.
+;; revert/update buffers automatically when underlying files are changed externally.
 (global-auto-revert-mode t)
 
-(fset 'yes-or-no-p 'y-or-n-p)      ; y and n instead of yes and no everywhere else
+;; y and n instead of yes and no everywhere else
+(fset 'yes-or-no-p 'y-or-n-p)
 (delete-selection-mode 1)
 
 ;; don't backupfiles
@@ -101,93 +141,15 @@
 ;; i hate lockfiles, aka .#file
 (setq create-lockfiles nil)
 
-;; save a list of recent files visited. (open recent file with C-x f)
-(recentf-mode 1)
-
-;; removes nasty bell
-(setq ring-bell-function 'ignore)
-
-;; no blinking cursor
-(blink-cursor-mode -1)
-
 ;; never insert tabs
 (set-default 'indent-tabs-mode nil)
 
-;; minimal ui
-;; show me empty lines after buffer end
-(set-default 'indicate-empty-lines t)
-(setq-default truncate-lines t)
-
-;; see matching pairs of parentheses and other characters
-(show-paren-mode t)
-
-(use-package dracula-theme
-  :init
-  (load-theme 'dracula t)
+;; vim
+(use-package evil
   :ensure t)
 
-(use-package rainbow-delimiters
-  :init
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-  :ensure t)
-
-;; default font size
-(set-face-attribute 'default nil :height 180)
-
-(global-visual-line-mode 1)
-
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
-
-(setq frame-title-format '("%b (%m)"))
-
-
-(use-package ivy
-  :init
-  (ivy-mode 1)
-  :ensure t)
-
-(use-package counsel
-  :ensure t)
-
-(use-package avy
-  :ensure t)
-
-(use-package expand-region
-  :ensure t)
-
-(use-package multiple-cursors
-  :ensure t)
-
-(use-package undo-tree
-  :config (global-undo-tree-mode)
-  :ensure t)
-
-(use-package git-gutter
-  :config (global-git-gutter-mode +1)
-  :ensure t)
-
-(use-package git-timemachine
-  :ensure t)
-
-(use-package rainbow-delimiters
-  :init
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-  :ensure t)
-
-(global-visual-line-mode 1)
-
-(setq ivy-use-virtual-buffers t)
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-(use-package magit
-  :ensure t)
-
-(use-package whitespace-cleanup-mode
-  :init
-  (global-whitespace-cleanup-mode t)
+;; find and replace
+(use-package anzu
   :ensure t)
 
 ;; auto save on lost focus
@@ -202,14 +164,43 @@
                                               mode-enabled))
   :ensure t)
 
-;; vim
-(use-package evil
+(use-package whitespace-cleanup-mode
+  :init
+  (global-whitespace-cleanup-mode t)
   :ensure t)
 
-;; find and replace
-(use-package anzu
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(use-package undo-tree
+  :config (global-undo-tree-mode)
   :ensure t)
 
+(use-package avy
+  :ensure t)
+
+(use-package expand-region
+  :ensure t)
+
+(use-package multiple-cursors
+  :ensure t)
+
+(use-package smartparens
+  :init (smartparens-global-mode)
+  :ensure t)
+
+;;;;;
+;; git
+(use-package magit
+  :ensure t)
+
+(use-package git-gutter
+  :config (global-git-gutter-mode +1)
+  :ensure t)
+
+(use-package git-timemachine
+  :ensure t)
+
+;;;;;
 ;; bindings
 (cua-mode t)
 
@@ -240,10 +231,9 @@
 
 (global-set-key (kbd "s-l") 'er/expand-region)
 
-(global-set-key (kbd "s-N") 'switch-to-scratch)
 (global-set-key (kbd "s-n") 'crux-create-scratch-buffer)
 
-(global-set-key (kbd "C-tab") 'crux-other-window-or-switch-buffer)
+(global-set-key (kbd "<C-tab>") 'crux-other-window-or-switch-buffer)
 
 (global-set-key (kbd "s-r") 'counsel-imenu)
 (global-set-key (kbd "s-e") 'evil-mode)
@@ -269,19 +259,3 @@
 (global-set-key (kbd "<f8>") 'global-aggressive-indent-mode)
 (global-set-key (kbd "<f9>") 'indent-buffer)
 (global-set-key (kbd "<f10>") 'magit-status)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("c48551a5fb7b9fc019bf3f61ebf14cf7c9cdca79bcb2a4219195371c02268f11" default))
- '(package-selected-packages
-   '(anzu company-statistics company-flx company smartparens which-key crux realgud exec-path-from-shell flycheck-julia julia-repl docker julia-mode zenburn-theme yalinum whitespace-cleanup-mode use-package undo-tree sublime-themes subatomic-theme selectrum-prescient rainbow-delimiters multiple-cursors magit ivy-rich guide-key git-timemachine git-gutter flycheck expand-region evil counsel consult-selectrum centaur-tabs avy)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background nil)))))
